76609af3ecc061d4d3e89bfcc15b932f6d2d80a4
dayglow-chorock <dayglow@chorock.co.kr>
2025-02-10 10:52:02 +0900

    feat: 차트 시간 관련 상수 값 추가

---
+++ src/constants/charDate.js
@@ -0,0 +1,6 @@
+export const HOUR = "1D";
+export const DAY = "7D";
+export const ONE_HOUR = 1000 * 60 * 60;
+export const TEN_MINUTES = 1000 * 60 * 10;
+export const tickInterval = ONE_HOUR;
+export const KST_OFFSET = 9 * 60 * 60 * 1000;
